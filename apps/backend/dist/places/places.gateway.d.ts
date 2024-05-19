931e416c0b29365cf90c2d4ea196c0cc08d509c0
Ziyad <137479354+ZiyadBoshima@users.noreply.github.com>
2024-05-19 23:04:59 +0200

    remove dist folder

--- apps/backend/dist/places/places.gateway.d.ts
+++
@@ -1,7 +0,0 @@
-import { OnModuleInit } from "@nestjs/common";
-import { Server, Socket } from 'socket.io';
-export declare class PlacesGateway implements OnModuleInit {
-    server: Server;
-    onModuleInit(): void;
-    onRequestPlacesDetails(client: Socket, body: any): void;
-}

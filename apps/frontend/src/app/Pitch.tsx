db69098b10c729aef1631cad4dd8f0abf4abef2f
Ziyad <137479354+ZiyadBoshima@users.noreply.github.com>
2024-05-20 00:22:17 +0200

    create ui components

---
+++ apps/frontend/src/app/Pitch.tsx
@@ -0,0 +1,15 @@
+const Pitch = () => {
+  return (
+    <div className="border rounded-3xl border-black py-3 px-5">
+      <p className="font-semibold">Hamburg</p>
+      <div className="mt-2 max-h-52 overflow-auto w-full">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A explicabo consequatur quo, magnam ducimus voluptatibus distinctio? Sint nihil nemo quas minima aspernatur eligendi! Id nemo vero magni dolor possimus saepe.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A explicabo consequatur quo, magnam ducimus voluptatibus distinctio? Sint nihil nemo quas minima aspernatur eligendi! Id nemo vero magni dolor possimus saepe.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A explicabo consequatur quo, magnam ducimus voluptatibus distinctio? Sint nihil nemo quas minima aspernatur eligendi! Id nemo vero magni dolor possimus saepe.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A explicabo consequatur quo, magnam ducimus voluptatibus distinctio? Sint nihil nemo quas minima aspernatur eligendi! Id nemo vero magni dolor possimus saepe.
+      </div>
+    </div>
+  )
+}
+
+export default Pitch
--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + ' ' + 'm-auto max-w-screen-md p-10'}>
+        {children}
+      </body>
     </html>
   );
 }

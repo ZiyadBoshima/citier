7e2f388942e4f9a3ab379ac9b46692b48fee2218
Ziyad <137479354+ZiyadBoshima@users.noreply.github.com>
2024-05-20 13:17:58 +0200

    allow submit onEnter

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -16,7 +16,13 @@ export default function Home() {
     <main>
       <h2>Get your city pitch 🌆</h2>
       <div className="mt-2 flex gap-4">
-        <input onChange={(e) => setCityName(e.target.value)} value={cityName} className="border border-black p-1" type="text" placeholder="Enter a city name..." />
+        <input 
+          onChange={(e) => setCityName(e.target.value)}
+          onKeyDown={e => e.key === 'Enter' ? onSubmit() : ''}
+          value={cityName} className="border border-black p-1"
+          type="text" 
+          placeholder="Enter a city name..." 
+        />
         <button onClick={onSubmit} className="bg-blue-400 text-white p-2">Get city pitch</button>
       </div>
       <div className="mt-10 flex flex-col gap-5">

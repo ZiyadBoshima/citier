eedf58796531b9cbb5f3137b64a64d566b3b2a82
Ziyad <137479354+ZiyadBoshima@users.noreply.github.com>
2024-05-20 02:07:42 +0200

    make use of the mongo_uri env

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,11 +3,17 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PlacesModule } from './places/places.module';
 import { OpenaiModule } from './openai/openai.module';
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [PlacesModule, OpenaiModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/'), ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV}` })],
+  imports: [PlacesModule, OpenaiModule, ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV}` }), 
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({uri: config.get('MONGO_URI')}),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
